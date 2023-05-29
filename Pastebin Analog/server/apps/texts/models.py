ff2cb60d62374faf08a1bebd260340235f58c321
Madjar Ivan <evanmadjar@gmail.com>
2023-05-29 11:01:32 +0300

    feat: Add hash field to TextBlock model

--- Pastebin Analog/server/apps/texts/models.py
+++ Pastebin Analog/server/apps/texts/models.py
@@ -1,12 +1,25 @@
+import random
 from django.db import models
+from django.conf import settings
 from common.mixins.models import BaseModel
 from users.models import User
+
+
+def _create_hash() -> str:
+    letters: str = settings.HASH_ALPHABET
+    length: int = settings.DEFAULT_HASH_LENGTH
+    random_hash: str = ''.join(random.choice(letters)\
+                               for _ in range(length))
+    return random_hash
 
 
 class TextBlock(BaseModel):
     text = models.TextField(max_length=1048)
     author = models.ForeignKey(
         to=User, on_delete=models.SET_NULL, blank=True, null=True)
+    hash = models.CharField(
+        max_length=settings.DEFAULT_HASH_LENGTH,
+        default=_create_hash, unique=True)
 
     class Meta:
         verbose_name = 'Text block'
