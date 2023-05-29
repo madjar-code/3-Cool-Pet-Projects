f2bc504a271de99cf8575080230fe3fe4423692c
Madjar Ivan <evanmadjar@gmail.com>
2023-05-29 13:02:42 +0300

    feat: Create serializers for TextBlock

---
+++ Pastebin Analog/server/apps/texts/api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from rest_framework.serializers import\
+    ModelSerializer
+
+from texts.models import TextBlock
+
+
+class SimpleTextBlockSerializer(ModelSerializer):
+    class Meta:
+        model = TextBlock
+        fields = (
+            'id',
+            'author',
+            'hash',
+        )
+        read_only_fields = fields
+
+
+class TextBlockSerializer(ModelSerializer):
+    class Meta:
+        model = TextBlock
+        fields = (
+            'id',
+            'author',
+            'hash',
+            'text',
+        )
