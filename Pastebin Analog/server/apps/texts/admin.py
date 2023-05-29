bca89de020bdcb4d41ffabec36b3b34f909a4189
Madjar Ivan <evanmadjar@gmail.com>
2023-05-29 11:14:07 +0300

    feat: Register TextBlock in admin panel

--- Pastebin Analog/server/apps/texts/admin.py
+++ Pastebin Analog/server/apps/texts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from common.mixins.admin import BaseAdmin
+from .models import TextBlock
 
-# Register your models here.
+@admin.register(TextBlock)
+class TextBlockAdmin(BaseAdmin):
+    list_display = (
+        'short_text',
+        'author',
+        'hash',
+        'is_active',
+    )
+    search_fields = (
+        'author',
+    )
+    list_filter = (
+        'author',
+        'text',
+        'is_active',
+    )
+
+    def short_text(self, obj: TextBlock) -> str:
+        return obj.text[:40] + '...' if len(obj.text) > 40\
+                                     else obj.text
+    short_text.short_description = 'Text'
