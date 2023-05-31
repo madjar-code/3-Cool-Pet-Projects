c262116c8a0f6a111c6477940fbd0b3018a91afa
Madjar Ivan <evanmadjar@gmail.com>
2023-05-31 10:44:49 +0300

    chore: Change '-' to '_' in url names

--- Pastebin Analog/server/apps/texts/api/urls.py
+++ Pastebin Analog/server/apps/texts/api/urls.py
@@ -7,9 +7,8 @@ from .views import (
 
 app_name = 'texts'
 
-
 urlpatterns = [
-    path('', TextBlockListView.as_view(), name='text-block-list'),
-    path('user-<str:username>/', TextsForUser.as_view(), name='text-blocks-for-user'),
-    path('<str:hash>/', TextBlockDetailsView.as_view(), name='text-block-detail'),
+    path('', TextBlockListView.as_view(), name='text_block_list'),
+    path('user-<str:username>/', TextsForUser.as_view(), name='text_blocks_for_user'),
+    path('<str:hash>/', TextBlockDetailsView.as_view(), name='text_block_detail'),
 ]
