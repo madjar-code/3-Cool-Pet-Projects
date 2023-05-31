a51880da3c4ca3665e54b4e7e245a8415bbdffb1
Madjar Ivan <evanmadjar@gmail.com>
2023-05-31 10:45:41 +0300

    feat: Replaced the random module with secrets

--- Pastebin Analog/server/apps/texts/models.py
+++ Pastebin Analog/server/apps/texts/models.py
@@ -1,4 +1,4 @@
-import random
+import secrets
 from django.db import models
 from django.conf import settings
 from common.mixins.models import BaseModel
@@ -8,15 +8,15 @@ from users.models import User
 def _create_hash() -> str:
     letters: str = settings.HASH_ALPHABET
     length: int = settings.DEFAULT_HASH_LENGTH
-    random_hash: str = ''.join(random.choice(letters)\
-                               for _ in range(length))
+    random_hash: str = ''.join(secrets.choice(letters) for _ in range(length))
     return random_hash
 
 
 class TextBlock(BaseModel):
     text = models.TextField(max_length=2096)
     author = models.ForeignKey(
-        to=User, on_delete=models.SET_NULL, blank=True, null=True)
+        to=User, on_delete=models.SET_NULL,
+        blank=True, null=True, related_name='text_blocks')
     hash = models.CharField(
         max_length=settings.DEFAULT_HASH_LENGTH,
         default=_create_hash, unique=True)
