bd6f87443e263c6dafcfd1ca1e9fdab3ef222ca0
Madjar Ivan <evanmadjar@gmail.com>
2023-05-31 10:45:22 +0300

    refactor: Test refactoring

--- Pastebin Analog/server/apps/texts/tests/test_views.py
+++ Pastebin Analog/server/apps/texts/tests/test_views.py
@@ -1,53 +1,48 @@
 from mixer.backend.django import mixer
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.test import APITestCase
+from django.urls import reverse
 
 from users.models import User
 from texts.models import TextBlock
 
 
 class TestTextBlockViews(APITestCase):
-    def setUp(self) -> None:
-        self.user = User.objects.create_user(
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
             username='test', email='test@test.com', password='test')
-        self.text_block1: TextBlock = mixer.blend(
-            TextBlock, author=self.user, text=mixer.faker.text())
-        self.text_block2: TextBlock = mixer.blend(
-            TextBlock, author=self.user, text=mixer.faker.text())
-        self.text_block3: TextBlock = mixer.blend(
+        cls.text_block1 = mixer.blend(
+            TextBlock, author=cls.user, text=mixer.faker.text())
+        cls.text_block2 = mixer.blend(
+            TextBlock, author=cls.user, text=mixer.faker.text())
+        cls.text_block3 = mixer.blend(
             TextBlock, text=mixer.faker.text())
-        self.text_block4: TextBlock = mixer.blend(
+        cls.text_block4 = mixer.blend(
             TextBlock, text=mixer.faker.text())
 
-    def test_getting_text_block(self) -> None:
-        """
-        Get one text block by hash
-        """
-        hash: str = self.text_block1.hash
-        response: Response = self.client.get(f'/api/v1/text-blocks/{hash}/')
+    def get_text_block_url(self, hash):
+        return reverse('texts:text_block_detail', kwargs={'hash': hash})
+
+    def get_text_blocks_for_user_url(self, username):
+        return reverse('texts:text_blocks_for_user', kwargs={'username': username})
+
+    def test_getting_text_block(self):
+        url = self.get_text_block_url(self.text_block1.hash)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_getting_text_block_wrong_data(self) -> None:
-        """
-        Get one text block with wrong data
-        """
-        random_hash = '1' * 20
-        response: Response = self.client.get(f'/api/v1/text-blocks/{random_hash}/')
+    def test_getting_text_block_wrong_data(self):
+        url = self.get_text_block_url('1' * 20)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_getting_text_block_to_user(self) -> None:
-        """
-        Get text blocks to user by username
-        """
-        username: str = self.user.username
-        response: Response = self.client.get(f'/api/v1/text-blocks/user-{username}/')
+    def test_getting_text_blocks_for_user(self):
+        url = self.get_text_blocks_for_user_url(self.user.username)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_getting_text_block_to_user_wrong_data(self) -> None:
-        """
-        Get text blocks to user with wrong username
-        """
-        random_username = '1' * 20
-        response: Response = self.client.get(f'/api/v1/text-blocks/user-{random_username}/')
+    def test_getting_text_blocks_for_user_wrong_data(self):
+        url = self.get_text_blocks_for_user_url('1' * 20)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
