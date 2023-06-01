4e19dd4fc62533560efcfc043d435ed8d925239a
Madjar Ivan <evanmadjar@gmail.com>
2023-06-01 16:22:09 +0300

    feat: Create HashGenerator class

---
+++ Pastebin Analog/server/apps/texts/services/hash.py
@@ -0,0 +1,30 @@
+import secrets
+from django.conf import settings
+from texts.models import TextBlock
+
+
+HASH_ALPHABET: str = settings.HASH_ALPHABET
+DEFAULT_HASH_LENGTH: int = settings.DEFAULT_HASH_LENGTH
+MAX_ATTEMPTS: int = 1000
+
+
+class MaxAttemptsError(Exception):
+    pass
+
+
+class HashGenerator:
+    def _create_hash(self) -> str:
+        random_hash: str = ''.join(secrets.choice(HASH_ALPHABET)
+                              for _ in range(DEFAULT_HASH_LENGTH))
+        return random_hash
+    
+    def create_unique_hash(self, attempt_number: int = 0) -> str:
+        if attempt_number >= MAX_ATTEMPTS:
+            raise MaxAttemptsError(
+                'Too many attempts to create a unique'
+                'hash. You may have run out of free hashes')
+        random_hash: str = self._create_hash()
+
+        if TextBlock.objects.filter(hash=random_hash).exists():
+            return self.create_unique_hash()
+        return random_hash
