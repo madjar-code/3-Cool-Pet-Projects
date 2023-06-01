54b7821739eb023809dcce3220bb109b1eeefe7e
Madjar Ivan <evanmadjar@gmail.com>
2023-06-01 11:10:11 +0300

    test: Make test for CreateTextBlockSerializer

--- Pastebin Analog/server/apps/texts/tests/test_serializers.py
+++ Pastebin Analog/server/apps/texts/tests/test_serializers.py
@@ -1,4 +1,3 @@
-import uuid
 from typing import List, OrderedDict, Any
 from django.test import TestCase
 from mixer.backend.django import mixer
@@ -7,13 +6,15 @@ from texts.models import TextBlock
 from texts.api.serializers import (
     SimpleTextBlockSerializer,
     TextBlockSerializer,
+    CreateTextBlockSerializer,
 )
 
 
 class TestTextBlockSerializers(TestCase):
     def setUp(self) -> None:
-        self.user = User.objects.create_user(
+        self.user: User = User.objects.create_user(
             username='test', email='test@test.com', password='test')
+
         self.text_block1: TextBlock = mixer.blend(
             TextBlock, author=self.user, text=mixer.faker.text())
         self.text_block2: TextBlock = mixer.blend(
@@ -29,6 +30,16 @@ class TestTextBlockSerializers(TestCase):
             instance=self.text_block1)
         self.detail_serializer2 = TextBlockSerializer(instance=self.text_block3)
 
+        self.create_serializer1 = CreateTextBlockSerializer(
+            data={'text': 'Test Text'})
+        self.create_serializer2 = CreateTextBlockSerializer(
+            data={'text': 'Test Text', 'expiration_time': '2023-06-01T07:25:39.447Z'})
+        self.create_serializer3 = CreateTextBlockSerializer(
+            data={'text': 'Test Text', 'author': self.user.id})
+        self.create_serializer4 = CreateTextBlockSerializer(
+            data={'text': 'Test Text', 'expiration_time': 'wrong_time'})
+        
+
     def test_simple_serializer(self):
         serialized_data: List[OrderedDict[str, Any]] = self.simple_serializer.data
         self.assertEqual(len(serialized_data), 2)
@@ -40,4 +51,23 @@ class TestTextBlockSerializers(TestCase):
         serialized_data: OrderedDict[str, Any] = self.detail_serializer1.data
     
         self.assertEqual(list(serialized_data.keys()),
-                         ['id', 'author', 'hash', 'text'])
+                         ['id', 'author', 'hash', 'text', 'expiration_time'])
+
+    def test_create_serializer_only_text(self):
+        serializer: CreateTextBlockSerializer = self.create_serializer1
+        self.assertTrue(serializer.is_valid())
+
+    def test_create_serializer_text_exp_time(self):
+        serializer: CreateTextBlockSerializer = self.create_serializer2
+        self.assertTrue(serializer.is_valid())
+
+    def test_create_serializer_text_author(self):
+        serializer: CreateTextBlockSerializer = self.create_serializer3
+        self.assertTrue(serializer.is_valid())
+
+    def test_create_serializer_text_exp_time_wrong_data(self):
+        serializer: CreateTextBlockSerializer = self.create_serializer4
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('expiration_time', serializer.errors)
+        error_message = serializer.errors['expiration_time'][0]
+        self.assertEqual(error_message, 'Invalid date format')
