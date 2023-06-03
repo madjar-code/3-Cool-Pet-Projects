2656c49236fedbea56a5d8cb152d0daa2c5b6b28
Madjar Ivan <evanmadjar@gmail.com>
2023-06-03 12:44:04 +0300

    refactor: Remove generate_hashes method

--- Pastebin Analog/server/apps/texts/services/hash.py
+++ Pastebin Analog/server/apps/texts/services/hash.py
@@ -8,7 +8,6 @@ from texts.models import TextBlock
 HASH_ALPHABET: str = settings.HASH_ALPHABET
 DEFAULT_HASH_LENGTH: int = settings.DEFAULT_HASH_LENGTH
 MAX_ATTEMPTS = 1000
-CACHE_KEY = 'hash_generator_cache_key'
 
 
 class MaxAttemptsError(Exception):
@@ -21,13 +20,6 @@ class HashGenerator:
                               for _ in range(DEFAULT_HASH_LENGTH))
         return random_hash
 
-    def generate_hashes(self, num_hashes: int = 20) -> None:
-        hashes: List[str] = []
-        for _ in range(num_hashes):
-            hash_value = self.create_unique_hash()
-            hashes.append(hash_value)
-        cache.set(CACHE_KEY, hashes)
-            
     def create_unique_hash(self, attempt_number: int = 0) -> str:
         if attempt_number >= MAX_ATTEMPTS:
             raise MaxAttemptsError(
