d044035c23055a0b5c9f7c7454297de95f94477b
Madjar Ivan <evanmadjar@gmail.com>
2023-06-06 09:49:01 +0300

    feat: Register CreateNote page

--- Pastebin Analog/client/src/App.js
+++ Pastebin Analog/client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import CreateNote from './pages/CreateNote/CreateNote';
 
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path='/' element={<CreateNote/>}/>
+      </Routes>
+    </Router>
   );
 }
 
