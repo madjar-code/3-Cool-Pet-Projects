c83a64fb06822b3b4f0749aa718f2ad5ce1cd7d4
Madjar Evan <evanmadjar@gmail.com>
2023-06-14 18:07:26 +0300

    feat: Add new logging

--- Pastebin Analog/server/server/settings/base.py
+++ Pastebin Analog/server/server/settings/base.py
@@ -1,7 +1,11 @@
 import os, sys, environ
+import logging
+import logging.config
 from datetime import timedelta
 from pathlib import Path
 from string import ascii_letters, digits
+from django.utils.log import DEFAULT_LOGGING
+
 
 env = environ.Env()
 
@@ -63,7 +67,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     # logging
-    'server.logging.middleware.RequestTimeMiddleware',
+    # 'server.logging.middleware.RequestTimeMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -179,3 +183,39 @@ DEFAULT_HASH_LENGTH = 10
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = 'INFO'
+
+logging.config.dictConfig(
+    {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'console': {
+                'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+            },
+            'file': {'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'},
+            'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'console',
+            },
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'formatter': 'file',
+                'filename': 'logging/app.log',
+            },
+            'django.server': DEFAULT_LOGGING['handlers']['django.server'],
+        },
+        'loggers': {
+            '': {'level': 'INFO', 'handlers': ['console', 'file'], 'propagate': False},
+            'apps': {'level': 'INFO', 'handlers': ['console'], 'propagate': False},
+            'django.server': DEFAULT_LOGGING['loggers']['django.server'],
+        }
+    }
+)
