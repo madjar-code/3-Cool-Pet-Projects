f2bcac21bc4edab4b3851acd4f089d00759e4069
Madjar Evan <evanmadjar@gmail.com>
2023-06-16 16:12:23 +0300

    chore: Remove database code from settings

--- Pastebin Analog/server/server/settings/base.py
+++ Pastebin Analog/server/server/settings/base.py
@@ -101,13 +101,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = []
 
 SIMPLE_JWT = {
