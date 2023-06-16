318414ea4a8d73784ca9203edb4d67ed3cc22dc5
Madjar Evan <evanmadjar@gmail.com>
2023-06-16 12:53:44 +0300

    chore: Add Redis cache backend

--- Pastebin Analog/server/server/settings/development.py
+++ Pastebin Analog/server/server/settings/development.py
@@ -7,3 +7,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
