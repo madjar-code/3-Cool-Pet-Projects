48fa62515de519a10ee98d7b3817d7ae78042e5e
Madjar Evan <evanmadjar@gmail.com>
2023-06-21 11:21:20 +0300

    feat: Register endpoints

--- ENS/server/apps/contacts/api/urls.py
+++ ENS/server/apps/contacts/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import (
     ContactsListView,
+    ContactDetailsView,
+    CreateContactView,
 )
 
 
@@ -9,4 +11,6 @@ app_name = 'contacts'
 
 urlpatterns = [
     path('', ContactsListView.as_view(), name='contacts_list'),
+    path('create/', CreateContactView.as_view(), name='create_contact'),
+    path('<str:id>/', ContactDetailsView.as_view(), name='contact_details'),
 ]
