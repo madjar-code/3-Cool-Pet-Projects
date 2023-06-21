43ee75b03310c4b08470ba9c1b4efe129f45f737
Madjar Evan <evanmadjar@gmail.com>
2023-06-21 10:34:37 +0300

    feat: Add Contact model to admin panel

--- ENS/server/apps/contacts/admin.py
+++ ENS/server/apps/contacts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from common.mixins.admin import BaseAdmin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(BaseAdmin):
+    list_display = (
+        'id',
+        'name',
+        'email',
+        'phone',
+        'priority_group',
+        'created_at',
+        'is_active',
+    )
+    list_filter = (
+        'priority_group',
+    )
+    search_fields = (
+        'name',
+        'email',
+        'phone',
+    )
+    list_display_links = (
+        'id',
+        'name',
+    )
+    model = Contact
