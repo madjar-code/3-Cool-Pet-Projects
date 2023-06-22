57cbad074d211f0c191efe927afb3e5c0c7dd17c
Madjar Evan <evanmadjar@gmail.com>
2023-06-22 14:20:42 +0300

    feat: Add NotificationTemplate to admin panel

--- ENS/server/apps/notifications/admin.py
+++ ENS/server/apps/notifications/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import NotificationTemplate
 
-# Register your models here.
+
+@admin.register(NotificationTemplate)
+class NotificationTemplateAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'text',
+        'send_time',
+    )
+    list_filter = (
+        'created_at',
+    )
+    search_fields = (
+        'title',
+        'text',
+        'send_time',
+    )
+    list_display_links = (
+        'id',
+        'title',
+    )
+    model = NotificationTemplate
