c1eb69a94434c34bd1935d920a748321b7b3933a
Madjar Evan <evanmadjar@gmail.com>
2023-06-22 11:21:54 +0300

    feat: Create an endpoint to parse the contacts

--- ENS/server/apps/contacts/api/urls.py
+++ ENS/server/apps/contacts/api/urls.py
@@ -3,6 +3,7 @@ from .views import (
     ContactsListView,
     ContactDetailsView,
     CreateContactView,
+    UploadContactsSheetView,
 )
 
 
@@ -12,5 +13,6 @@ app_name = 'contacts'
 urlpatterns = [
     path('', ContactsListView.as_view(), name='contacts_list'),
     path('create/', CreateContactView.as_view(), name='create_contact'),
+    path('upload-excel/', UploadContactsSheetView.as_view(), name='upload_sheet'),
     path('<str:id>/', ContactDetailsView.as_view(), name='contact_details'),
 ]
--- ENS/server/apps/contacts/api/views.py
+++ ENS/server/apps/contacts/api/views.py
@@ -1,15 +1,24 @@
+import os
 from enum import Enum
+from django.core.files import File
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.parsers import (
+    FileUploadParser,
+    MultiPartParser,
+)
 from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView,
     CreateAPIView,
 )
 from rest_framework.permissions import IsAdminUser
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from contacts.models import Contact
+from openpyxl import load_workbook
 from .serializers import (
     SimpleContactSerializer,
     ContactSerializer,
@@ -20,6 +29,7 @@ from .serializers import (
 class ErrorMessages(str, Enum):
     NO_USER = 'User with `username` not found'
     NO_CONTACT = 'Contact with `id` not found'
+    INVALID_TYPE = 'Invalid file type. Only Excel files (.xlsx) are supported.'
 
 
 class ContactsListView(ListAPIView):
@@ -60,3 +70,52 @@ class CreateContactView(CreateAPIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
+class UploadContactsSheetView(APIView):
+    parser_classes = (MultiPartParser,)
+
+    @swagger_auto_schema(
+        operation_id='upload_sheet',
+        manual_parameters=[
+            openapi.Parameter(
+                'file',
+                in_=openapi.IN_FORM,
+                type=openapi.TYPE_FILE,
+                required=True,
+                description='Excel sheet file'
+            ),
+        ]
+    )
+    def post(self, request: Request) -> Response:
+        file_object: File = request.data['file']
+        file_name = file_object.name
+        file_extension = os.path.splitext(file_name)[1].lower()
+
+        if file_extension != '.xlsx':
+            return Response({'error': ErrorMessages.INVALID_TYPE.value},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        wb = load_workbook(file_object)
+        sheet = wb.active
+
+        contacts = []
+
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            name = row[0]
+            email = row[1]
+            phone = row[2]
+            priority_group = row[3]
+
+            contact_data = {
+                'name': name,
+                'email': email,
+                'phone': phone,
+                'priority_group': priority_group,
+            }
+
+            serializer = CreateContactSerializer(data=contact_data)
+            serializer.is_valid(raise_exception=True)
+            contact: CreateContactSerializer = serializer.save()
+            contacts.append(contact.data)
+
+        return Response({'contacts': contacts})
