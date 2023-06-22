49ce70f71e9e97d4f88d20d1cd54b21cd972cdb4
Madjar Evan <evanmadjar@gmail.com>
2023-06-22 13:59:46 +0300

    chore: Register endpoints in urls.py

--- ENS/server/apps/contacts/api/urls.py
+++ ENS/server/apps/contacts/api/urls.py
@@ -3,6 +3,8 @@ from .views import (
     ContactsListView,
     ContactDetailsView,
     CreateContactView,
+    DeleteContactView,
+    UpdateContactView,
     UploadContactsSheetView,
 )
 
@@ -14,5 +16,7 @@ urlpatterns = [
     path('', ContactsListView.as_view(), name='contacts_list'),
     path('create/', CreateContactView.as_view(), name='create_contact'),
     path('upload-excel/', UploadContactsSheetView.as_view(), name='upload_sheet'),
+    path('delete/<str:id>/', DeleteContactView.as_view(), name='delete_contact'),
+    path('update/<str:id>/', UpdateContactView.as_view(), name='update_contact'),
     path('<str:id>/', ContactDetailsView.as_view(), name='contact_details'),
 ]
