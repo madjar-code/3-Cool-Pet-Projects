2e8f94e0e23efce7720fe637220270876b83207b
Madjar Evan <evanmadjar@gmail.com>
2023-06-23 12:23:00 +0300

    chore: Connect to PostgresQL database

--- ENS/server/server/settings/development.py
+++ ENS/server/server/settings/development.py
@@ -17,8 +17,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': env('POSTGRES_ENGINE'),
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('PG_HOST'),
+        'PORT': env('PG_PORT'),
     }
 }
 
