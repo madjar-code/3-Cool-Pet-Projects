6ed1cb95a24cdbe319adf007e154bd333ffa2e52
Madjar Evan <evanmadjar@gmail.com>
2023-06-24 10:20:50 +0300

    chore: Update settings and .env.example

--- ENS/server/server/settings/development.py
+++ ENS/server/server/settings/development.py
@@ -1,3 +1,4 @@
+from django.db.models import Model
 from .base import *
 
 
@@ -18,11 +19,54 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': env('POSTGRES_ENGINE'),
-        'NAME': env('POSTGRES_DB'),
         'USER': env('POSTGRES_USER'),
         'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': env('PG_HOST'),
-        'PORT': env('PG_PORT'),
-    }
+        'NAME': env('MASTER_DB_NAME'),
+        'HOST': env('MASTER_DB_HOST'),
+        'PORT': env('MASTER_DB_PORT'),
+    },
+
+    # 'replica': {
+    #     'ENGINE': env('POSTGRES_ENGINE'),
+    #     'USER': env('POSTGRES_USER'),
+    #     'PASSWORD': env('POSTGRES_PASSWORD'),
+    #     'NAME': env('REPLICA_DB_NAME'),
+    #     'HOST': env('REPLICA_DB_HOST'),
+    #     'PORT': env('REPLICA_DB_PORT'),
+    # },
 }
 
+
+# USE_REPLICA_DATABASE = env('USE_REPLICA_DATABASE')
+
+
+# class ReplicaRouter:
+#     def db_for_read(self, model, **hints):
+#         """
+#         Reads go to the replica.
+#         """
+#         if USE_REPLICA_DATABASE:
+#             return 'replica'
+#         return None
+
+#     def db_for_write(self, model, **hints):
+#         """
+#         Writes always go to primary.
+#         """
+#         return 'default'
+
+#     def allow_relation(self, object_1: Model, object_2: Model, **hints):
+#         """
+#         Relations between objects are allowed if both are in the
+#         primary/replica pool.
+#         """
+#         db_set = {'default', 'replica'}
+#         if object_1._state.db in db_set and object_2._state.db in db_set:
+#             return True
+#         return None
+
+#     def allow_migrate(self, db, app_label, model_name=None, **hints):
+#         return True
+
+
+# DATABASE_ROUTERS=[ReplicaRouter]
