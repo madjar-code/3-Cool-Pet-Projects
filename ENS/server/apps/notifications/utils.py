dbda9d2b920ee3a0f8f6687b0b94aa8e57c97151
Madjar Evan <evanmadjar@gmail.com>
2023-06-25 15:21:36 +0300

    feat: Create two functions to send emails

--- ENS/server/apps/notifications/utils.py
+++ ENS/server/apps/notifications/utils.py
@@ -1,7 +1,11 @@
+from time import sleep
+from typing import List
+from smtplib import SMTPException
 from django.contrib.auth.tokens import\
     PasswordResetTokenGenerator
 from django.utils.crypto import salted_hmac
 from django.utils.http import int_to_base36
+from django.core.mail import send_mail
 from notifications.models import NotificationTemplate
 
 
@@ -16,3 +20,17 @@ class NotificationTokenGenerator(PasswordResetTokenGenerator):
         hash_value = salted_hmac(self.key_salt, value).hexdigest()[::2]
 
         return f'{notification_template_id}-{timestamp}-{hash_value}'
+
+
+def dev_send_email(subject: str, body: str,
+                   recipient_list: List[str]) -> None:
+    send_mail(
+        subject, body, 'admin@admin.com',
+        recipient_list, fail_silently=False)
+    sleep(2)
+
+
+def dev_send_email_wrong(subject: str, body: str,
+                         recipient_list: List[str]) -> None:
+    sleep(2)
+    raise SMTPException
