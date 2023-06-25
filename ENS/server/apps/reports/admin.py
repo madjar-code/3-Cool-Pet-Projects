3840e40334d4f01da7da18846376ec2d988a8377
Madjar Evan <evanmadjar@gmail.com>
2023-06-25 14:54:23 +0300

    feat: Register NotificationState model in admin

--- ENS/server/apps/reports/admin.py
+++ ENS/server/apps/reports/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from common.mixins.admin import ReadOnlyFieldsAdmin
+from .models import NotificationState
 
-# Register your models here.
+
+@admin.register(NotificationState)
+class NotificationStateAdmin(ReadOnlyFieldsAdmin):
+    list_display = (
+        'short_id',
+        'contact',
+        'notification_template',
+        'status',
+        'method',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'status',
+        'method',
+    )
+    search_fields = (
+        'contact__name',
+        'notification_template__title',
+        'status',
+        'method',
+    )
+    list_display_links = (
+        'short_id',
+    )
+    
+    def short_id(self, obj):
+        return str(obj.id)[:7] + '...'
+
+    short_id.short_description = 'Short ID'
+
+    model = NotificationState
