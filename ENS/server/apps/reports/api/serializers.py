79b02af7e3a04550c5f5666a5c0cf09fc35e17d8
Madjar Evan <evanmadjar@gmail.com>
2023-07-03 11:13:29 +0300

    chore: Implement a serializer for list views

---
+++ ENS/server/apps/reports/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from reports.models import NotificationSession
+
+
+class SimpleNSSerializer(ModelSerializer):
+    class Meta:
+        model = NotificationSession
+        fields = (
+            'id',
+            'name',
+        )
+        read_only_fields = fields
