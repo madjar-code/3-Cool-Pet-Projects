b634235768ed747b481033f52a77d4f35107d6ce
Madjar Evan <evanmadjar@gmail.com>
2023-07-03 09:42:06 +0300

    feat: Setup Celery queues with different priority

--- ENS/server/server/celery.py
+++ ENS/server/server/celery.py
@@ -12,4 +12,26 @@ app = Celery("server")
 app.config_from_object("django.conf:settings", namespace="CELERY"),
 app.conf.broker_connection_retry_on_startup = True
 
+app.conf.task_queues = {
+    'low_priority_queue': {
+        'exchange': 'low_priority_queue',
+        'exchange_type': 'direct',
+        'routing_key': 'low_priority',
+    },
+    'high_priority_queue': {
+        'exchange': 'high_priority_queue',
+        'exchange_type': 'direct',
+        'routing_key': 'high_priority',
+    },
+}
+
+app.conf.task_routes = {
+    'notifications.tasks.send_notification': {
+        'queue': lambda task, *args, **kwargs: (
+            'high_priority_queue' if task.request.kwargs.get('priority_group') == 'high'\
+                                  else 'low_priority_queue'
+        ),
+    }
+}
+
 app.autodiscover_tasks(lambda: base.INSTALLED_APPS)
