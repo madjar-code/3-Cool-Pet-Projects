9b8d0498031d2677a34243f8a8ceee82f2d12049
Madjar Evan <evanmadjar@gmail.com>
2023-07-04 08:54:05 +0300

    feat: Update the counters in the task

--- ENS/server/apps/notifications/tasks.py
+++ ENS/server/apps/notifications/tasks.py
@@ -1,18 +1,18 @@
+from django.db import transaction
 from celery import shared_task
+from smtplib import SMTPException
+from django.db.models import F
+from twilio.base.exceptions import\
+    TwilioException
 from contacts.models import (
     Contact,
-    PriorityChoices,
 )
-from notifications.models import\
-    NotificationTemplate
 from reports.models import (
     MethodChoices,
-    StatusChoices,
+    StateStatusChoices,
     NotificationState,
     NotificationSession,
 )
-from smtplib import SMTPException
-from twilio.base.exceptions import TwilioException
 from notifications.utils import (
     dev_send_email,
     dev_send_sms,
@@ -29,39 +29,40 @@ def send_notification_by_method(method: MethodChoices, subject: str,
 
 @shared_task(time_limit=20)
 def send_notification(session_id: str,
-                      contact_id: str,
-                      priority_group: PriorityChoices) -> None:
-    notification_session = NotificationSession.\
-                objects.filter(id=session_id).first()
+                      contact_id: str) -> None:
+    notification_session = NotificationSession.objects.filter(id=session_id).first()
     contact: Contact = Contact.objects.filter(id=contact_id).first()
-    notification_state = NotificationState.objects.filter(
-        notification_session=notification_session, contact=contact
-    ).first()
+    
+    if not notification_session or not contact:
+        return
 
-    if not notification_state:
-        if contact.email:
-            method = MethodChoices.EMAIL_METHOD
-        elif contact.phone:
-            method = MethodChoices.PHONE_METHOD
-
-        notification_state = NotificationState.objects.create(
-            notification_session=notification_session,
-            status=StatusChoices.STATUS_DIRTY,
-            contact=contact,
-            method=method
-        )
-
-    method = notification_state.method
-
-    template = notification_session.notification_template
-    if notification_state.status == StatusChoices.STATUS_DIRTY:
+    notification_state, _ = NotificationState.objects.get_or_create(
+        notification_session=notification_session,
+        contact=contact,
+        defaults={
+            'status': StateStatusChoices.STATUS_DIRTY,
+            'method': MethodChoices.EMAIL_METHOD\
+                if contact.email else MethodChoices.PHONE_METHOD
+        }
+    )
+    if notification_state.status == StateStatusChoices.STATUS_DIRTY:
+        template = notification_session.notification_template
         subject: str = template.render_title()
         body: str = template.render_text()
         try:
-            send_notification_by_method(method, subject, body, contact)
-            notification_state.status = StatusChoices.STATUS_READY
+            NotificationSession.objects.filter(id=session_id).\
+                    update(during_counter=F('during_counter') + 1)
+            send_notification_by_method(
+                notification_state.method, subject, body, contact)
+            notification_state.status = StateStatusChoices.STATUS_READY
+            notification_state.save()
+            NotificationSession.objects.filter(id=session_id).\
+                update(success_counter=F('success_counter') + 1)
         except (SMTPException, TwilioException):
-            notification_state.status = StatusChoices.STATUS_FAILED
-        notification_state.save()
-
-    print(f"Задача send_notification выполнена для contact_id: {contact_id}")
+            notification_state.status = StateStatusChoices.STATUS_FAILED
+            notification_state.save()
+            NotificationSession.objects.filter(id=session_id).\
+                update(success_counter=F('failed_counter') + 1)
+        finally:
+            NotificationSession.objects.filter(id=session_id).\
+                update(during_counter=F('during_counter') - 1)
