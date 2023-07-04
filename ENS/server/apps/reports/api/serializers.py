446e55797988e7cf620379ee1a68f0693442483d
Madjar Evan <evanmadjar@gmail.com>
2023-07-04 11:39:11 +0300

    chore: Create serializers for report creation

--- ENS/server/apps/reports/api/serializers.py
+++ ENS/server/apps/reports/api/serializers.py
@@ -1,9 +1,43 @@
-from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from reports.models import NotificationSession
+from notifications.api.serializers import NTSerializer
+from reports.models import (
+    NotificationSession,
+    NotificationState,
+)
 
 
-class SimpleNSSerializer(ModelSerializer):
+class NStateSerializer(ModelSerializer):
+    class Meta:
+        model = NotificationState
+        fields = (
+            'contact',
+            'method',
+            'status',
+        )
+        read_only_fields = fields
+
+
+class ReportNSSerializer(ModelSerializer):
+    notification_template = NTSerializer()
+    states = NStateSerializer(many=True)
+    class Meta:
+        model = NotificationSession
+        fields = (
+            'id',
+            'name',
+            'status',
+            'success_counter',
+            'during_counter',
+            'failed_counter',
+            'all_counter',
+            'created_at',
+            'notification_template',
+            'states',
+        )
+        read_only_fields = fields
+
+
+class SimpleNSsessionSerializer(ModelSerializer):
     class Meta:
         model = NotificationSession
         fields = (
