7484e43a6d6c51263bddd586501f388dce93d1b3
Madjar Evan <evanmadjar@gmail.com>
2023-07-05 09:34:50 +0300

    feat: Add new queue for calculations

--- ENS/server/apps/reports/tasks.py
+++ ENS/server/apps/reports/tasks.py
@@ -16,7 +16,7 @@ class UUIDEncoder(json.JSONEncoder):
         return super().default(object)
 
 
-@shared_task(queue='low_priority_queue')
+@shared_task(queue='default')
 def create_report(session_id: UUID) -> None:
     session = NotificationSession.objects.filter(id=session_id).first()
     serializer = ReportNSSerializer(instance=session)
--- ENS/server/server/celery.py
+++ ENS/server/server/celery.py
@@ -1,37 +1,50 @@
 from __future__ import absolute_import
-
 import os
-
 from celery import Celery
+from kombu import Queue
 from server.settings import base
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings.development")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.development')
 
-app = Celery("server")
+app = Celery('server')
 
-app.config_from_object("django.conf:settings", namespace="CELERY"),
-app.conf.broker_connection_retry_on_startup = True
+default_queue = Queue('default', routing_key='default')
+low_priority_queue = Queue('low_priority_queue', routing_key='low_priority')
+high_priority_queue = Queue('high_priority_queue', routing_key='high_priority')
 
-app.conf.task_queues = {
-    'low_priority_queue': {
-        'exchange': 'low_priority_queue',
-        'exchange_type': 'direct',
-        'routing_key': 'low_priority',
-    },
-    'high_priority_queue': {
-        'exchange': 'high_priority_queue',
-        'exchange_type': 'direct',
-        'routing_key': 'high_priority',
-    },
-}
+app.conf.task_queues = [
+    default_queue,
+    low_priority_queue,
+    high_priority_queue,
+]
+
+# app.conf.task_queues = {
+#     'default': {
+#         'exchange': 'default',
+#         'exchange_type': 'direct',
+#         'routing_key': 'default',
+#     },
+#     'low_priority_queue': {
+#         'exchange': 'low_priority_queue',
+#         'exchange_type': 'direct',
+#         'routing_key': 'low_priority',
+#     },
+#     'high_priority_queue': {
+#         'exchange': 'high_priority_queue',
+#         'exchange_type': 'direct',
+#         'routing_key': 'high_priority',
+#     },
+# }
 
 app.conf.task_routes = {
     'notifications.tasks.send_notification': {
         'queue': lambda task, *args, **kwargs: (
-            'high_priority_queue' if task.request.kwargs.get('priority_group') == 'high'\
-                                  else 'low_priority_queue'
+            high_priority_queue if task.request.kwargs.get('priority_group') == 'high'\
+                                  else low_priority_queue
         ),
     }
 }
 
+app.config_from_object("django.conf:settings", namespace="CELERY"),
+app.conf.broker_connection_retry_on_startup = True
 app.autodiscover_tasks(lambda: base.INSTALLED_APPS)
