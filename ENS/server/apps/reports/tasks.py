363f3d4180618bf7f62922142c5d08bee889fbb3
Madjar Evan <evanmadjar@gmail.com>
2023-07-05 09:07:49 +0300

    feat: Implement task for file creation

---
+++ ENS/server/apps/reports/tasks.py
@@ -0,0 +1,31 @@
+import json
+from typing import Any
+from uuid import UUID
+from celery import shared_task
+from django.core.files.base import ContentFile
+from reports.models import NotificationSession
+from reports.api.serializers import (
+    ReportNSSerializer,
+)
+
+
+class UUIDEncoder(json.JSONEncoder):
+    def default(self, object: Any):
+        if isinstance(object, UUID):
+            return str(object)
+        return super().default(object)
+
+
+@shared_task(queue='low_priority_queue')
+def create_report(session_id: UUID) -> None:
+    session = NotificationSession.objects.filter(id=session_id).first()
+    serializer = ReportNSSerializer(instance=session)
+
+    json_data = json.dumps(serializer.data, cls=UUIDEncoder, indent=4)
+    filename = f'{session.id}.json'
+    
+    if session.report_file:
+        session.report_file.delete(save=False)
+
+    session.report_file.save(filename, ContentFile(json_data), save=False)
+    session.save()
