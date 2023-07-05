5acd89fe6df3b6e13ad79c232c47b8d4fc748a14
Madjar Evan <evanmadjar@gmail.com>
2023-07-05 13:16:26 +0300

    feat: Add check for two fields

--- ENS/server/apps/contacts/api/serializers.py
+++ ENS/server/apps/contacts/api/serializers.py
@@ -1,6 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from typing import (
+    Dict,
+    Any,
+)
+from enum import Enum
+from rest_framework import serializers
+from rest_framework.serializers import (
+    ModelSerializer,
+    ValidationError,
+)
 from contacts.models import Contact
 
+
+class ErrorMessages(str, Enum):
+    NO_PHONE_AND_EMAIL = "Either 'phone' or 'email' field is required."
 
 
 class SimpleContactSerializer(ModelSerializer):
@@ -45,8 +57,14 @@ class CreateContactSerializer(ModelSerializer):
             'created_at',
         )
 
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, Any]:
+        if 'phone' not in attrs and 'email' not in attrs:
+            raise ValidationError(ErrorMessages.NO_PHONE_AND_EMAIL.value)
+        return attrs
+
 
 class UpdateContactSerializer(ModelSerializer):
+    name = serializers.CharField(required=False)
     class Meta:
         model = Contact
         fields = (
