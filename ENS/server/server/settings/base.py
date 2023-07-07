277ae4264d3361fd32758bf9d6218b9920d1c7c4
Madjar Evan <evanmadjar@gmail.com>
2023-07-07 17:30:00 +0300

    build: Update the Docker files

--- ENS/server/server/settings/base.py
+++ ENS/server/server/settings/base.py
@@ -160,38 +160,38 @@ DEFAULT_SLUG_LENGTH = 7
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-LOG_LEVEL = 'INFO'
+# LOG_LEVEL = 'INFO'
 
-logging.config.dictConfig(
-    {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'console': {
-                'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-            },
-            'file': {'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'},
-            'django.server': DEFAULT_LOGGING['formatters']['django.server'],
-        },
-        'handlers': {
-            'console': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'console',
-            },
-            'file': {
-                'class': 'logging.FileHandler',
-                'level': 'INFO',
-                'formatter': 'file',
-                'filename': 'logging/app.log',
-            },
-            'django.server': DEFAULT_LOGGING['handlers']['django.server'],
-        },
-        'loggers': {
-            '': {'level': 'INFO', 'handlers': ['console', 'file'], 'propagate': False},
-            'apps': {'level': 'INFO', 'handlers': ['console'], 'propagate': False},
-            'django.server': DEFAULT_LOGGING['loggers']['django.server'],
-        }
-    }
-)
+# logging.config.dictConfig(
+#     {
+#         'version': 1,
+#         'disable_existing_loggers': False,
+#         'formatters': {
+#             'console': {
+#                 'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+#             },
+#             'file': {'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'},
+#             'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+#         },
+#         'handlers': {
+#             'console': {
+#                 'class': 'logging.StreamHandler',
+#                 'formatter': 'console',
+#             },
+#             'file': {
+#                 'class': 'logging.FileHandler',
+#                 'level': 'INFO',
+#                 'formatter': 'file',
+#                 'filename': 'logging/app.log',
+#             },
+#             'django.server': DEFAULT_LOGGING['handlers']['django.server'],
+#         },
+#         'loggers': {
+#             '': {'level': 'INFO', 'handlers': ['console', 'file'], 'propagate': False},
+#             'apps': {'level': 'INFO', 'handlers': ['console'], 'propagate': False},
+#             'django.server': DEFAULT_LOGGING['loggers']['django.server'],
+#         }
+#     }
+# )
