d51737bc9430470f463bbdca26266de6cb9e0f60
Madjar Evan <evanmadjar@gmail.com>
2023-07-07 12:37:36 +0300

    test: Add some tests and update test structures

--- ENS/server/apps/contacts/tests/test_serializers.py
+++ ENS/server/apps/contacts/tests/test_serializers.py
@@ -65,36 +65,52 @@ class TestContactSerializers(TestCase):
             },
         }
         
-        self.correct_update_data = [
-            # name
-            {
+        self.correct_update_data = {
+            'only_name': {
                 'name': 'Jenna'
             },
-            # phone
-            {
+            'only_phone': {
                 'phone': '+12125552368'
             },
-            # email
-            {
+            'only_email': {
                 'email': 'new@new.com'
             },
-            # priority
-            {
+            'only_priority_group': {
                 'priority_group': 'High'
             },
-            # phone and email
-            {
+            'email_and_phone': {
                 'phone': '+12125552368',
                 'email': 'new@new.com',
             },
-            # all fields
-            {
+            'all_fields': {
                 'name': 'Jenna',
                 'phone': '+12125552368',
                 'email': 'new@new.com',
                 'priority_group': 'High',
             }
-        ]
+        }
+        
+        self.wrong_update_data = {
+            'empty_name': {
+                'name': ''
+            },
+            'incorrect_phone': {
+                'phone': '+1212555236'
+            },
+            'incorrect_email': {
+                'email': 'newnew.com'
+            },
+            'no_phone_and_email': {
+                'email': '',
+                'phone': '',
+            },
+            'no_phone_with_no_email_instance': {
+                'phone': '',
+            },
+            'no_email_with_no_phone_instance': {
+                'email': '',
+            },
+        }
 
     def test_simple_serializer(self) -> None:
         contact_list = [self.contact1, self.contact2]
@@ -147,15 +163,87 @@ class TestContactSerializers(TestCase):
         self.assertEquals(list(serializer.errors.keys()), ['priority_group'])
 
     def test_update_serializer_name(self) -> None:
-        phone_data = self.correct_update_data[0]
-        serializer = UpdateContactSerializer(instance=self.contact1, data=phone_data)
+        name_data = self.correct_update_data['only_name']
+        serializer = UpdateContactSerializer(self.contact1, name_data)
         self.assertTrue(serializer.is_valid())
         updated_contact: Contact = serializer.save()
         self.assertEqual(str(updated_contact.name), 'Jenna')
 
     def test_update_serializer_phone(self) -> None:
-        phone_data = self.correct_update_data[1]
-        serializer = UpdateContactSerializer(instance=self.contact1, data=phone_data)
+        phone_data = self.correct_update_data['only_phone']
+        serializer = UpdateContactSerializer(self.contact1, phone_data)
         self.assertTrue(serializer.is_valid())
         updated_contact: Contact = serializer.save()
         self.assertEqual(str(updated_contact.phone), '+12125552368')
+
+    def test_update_serializer_email(self) -> None:
+        email_data = self.correct_update_data['only_email']
+        serializer = UpdateContactSerializer(self.contact1, email_data)
+        self.assertTrue(serializer.is_valid())
+        updated_contact: Contact = serializer.save()
+        self.assertEqual(str(updated_contact.email), 'new@new.com')
+
+    def test_update_serializer_priority(self) -> None:
+        priority_data = self.correct_update_data['only_priority_group']
+        serializer = UpdateContactSerializer(self.contact1, priority_data)
+        self.assertTrue(serializer.is_valid())
+        updated_contact: Contact = serializer.save()
+        self.assertEqual(str(updated_contact.priority_group), 'High')
+
+    def test_update_serializer_email_and_phone(self) -> None:
+        email_and_phone_data = self.correct_update_data['email_and_phone']
+        serializer = UpdateContactSerializer(self.contact1, email_and_phone_data)
+        self.assertTrue(serializer.is_valid())
+        updated_contact: Contact = serializer.save()
+        self.assertEqual(str(updated_contact.email), 'new@new.com')
+        self.assertEqual(str(updated_contact.phone), '+12125552368')
+
+    def test_update_serializer_all_fields(self) -> None:
+        all_fields = self.correct_update_data['all_fields']
+        serializer = UpdateContactSerializer(self.contact1, all_fields)
+        self.assertTrue(serializer.is_valid())
+        updated_contact: Contact = serializer.save()
+        self.assertEqual(str(updated_contact.name), 'Jenna')
+        self.assertEqual(str(updated_contact.email), 'new@new.com')
+        self.assertEqual(str(updated_contact.phone), '+12125552368')
+        self.assertEqual(str(updated_contact.priority_group), 'High')
+
+    def test_update_serializer_empty_name(self) -> None:
+        name_data = self.wrong_update_data['empty_name']
+        serializer = UpdateContactSerializer(self.contact1, name_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(list(serializer.errors.keys()), ['name'])
+
+    def test_update_serializer_incorrect_phone(self) -> None:
+        phone_data = self.wrong_update_data['incorrect_phone']
+        serializer = UpdateContactSerializer(self.contact1, phone_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(list(serializer.errors.keys()), ['phone'])
+
+    def test_update_serializer_incorrect_email(self) -> None:
+        phone_data = self.wrong_update_data['incorrect_email']
+        serializer = UpdateContactSerializer(self.contact1, phone_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(list(serializer.errors.keys()), ['email'])
+
+    def test_update_serializer_no_phone_and_email(self) -> None:
+        phone_email_data = self.wrong_update_data['no_phone_and_email']
+        serializer = UpdateContactSerializer(self.contact1, phone_email_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(list(serializer.errors.keys()), ['non_field_errors'])
+
+    def test_update_serializer_no_phone_with_no_email_instance(self) -> None:
+        phone_data = self.wrong_update_data['no_phone_with_no_email_instance']
+        self.contact1.email = None
+        self.contact1.save()
+        serializer = UpdateContactSerializer(self.contact1, phone_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(list(serializer.errors.keys()), ['non_field_errors'])
+
+    def test_update_serializer_no_email_with_no_phone_instance(self) -> None:
+        email_data = self.wrong_update_data['no_email_with_no_phone_instance']
+        self.contact1.phone = None
+        self.contact1.save()
+        serializer = UpdateContactSerializer(self.contact1, email_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(list(serializer.errors.keys()), ['non_field_errors'])
