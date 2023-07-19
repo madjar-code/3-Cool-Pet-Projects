b887a24f29f110584c9fd71fea205a4858f59e69
Madjar Evan <evanmadjar@gmail.com>
2023-07-19 20:55:53 +0300

    test: Test models using pytest

--- Pastebin Analog/server/apps/texts/tests/test_models.py
+++ Pastebin Analog/server/apps/texts/tests/test_models.py
@@ -1,51 +1,54 @@
-import time
-from datetime import timedelta
+import pytest
 from django.utils import timezone
-from django.test import override_settings
-from django.test.testcases import TestCase
-from django.conf import settings
 from users.models import User
-from texts.models import TextBlock
+from texts.models import TextBlock, Device
 
 
-class TestTextBlockModel(TestCase):
-    def setUp(self) -> None:
-        self.text1 =\
-            "Lorem Ipsum is simply dummy text "\
-            "of the printing and typesetting industry. "\
-            "Lorem Ipsum has been the industry's "\
-            "standard dummy text ever since the 1500s, "\
-            "when an unknown printer took a galley of "\
-            "type and scrambled it to make a type specimen book."
-        self.text2 =\
-            "Contrary to popular belief, "\
-            "Lorem Ipsum is not simply random text."
-        self.user = User.objects.create_user(
-            username='test', email='test@test.com', password='test')
-        self.text_block1 = TextBlock.objects.create(
-            author=self.user, text=self.text1)
-        self.text_block2 = TextBlock.objects.create(text=self.text2)
+@pytest.fixture
+def test_user(db):
+    user = User.objects.create_user(
+        username='testuser', email='test@test.com', password='testpass')
+    return user
 
-    def test_creation(self):
-        self.assertIsInstance(self.text_block1, TextBlock)
-        self.assertIsInstance(self.text_block2, TextBlock)
-        block1_repr = str(self.text_block1)
-        block2_repr = str(self.text_block2)
-        self.assertEqual(block1_repr, f"Text of {self.user.username}")
-        self.assertEqual(block2_repr, "Text of unknown user")
 
-    def test_hash_creation(self):
-        self.assertIsInstance(self.text_block1.hash, str)
-        self.assertEqual(len(self.text_block1.hash),
-                         settings.DEFAULT_HASH_LENGTH)
+@pytest.fixture
+def test_text_block(test_user: User):
+    text_block = TextBlock.objects.create(
+        title='Test Text',
+        text='This is a test text.',
+        author=test_user,
+        expiration_time=timezone.now() + timezone.timedelta(days=1),
+    )
+    return text_block
 
-    def test_text_block_exp_time(self):
-        now = timezone.now()
-        expiration_time = now + timedelta(seconds=1)
 
-        TextBlock.objects.create(
-            text='Test Text', expiration_time=expiration_time)
-        self.assertTrue(TextBlock.text_objects.all().exists())
-        time.sleep(1.2)
+def test_create_text_block(test_text_block: TextBlock):
+    assert TextBlock.objects.count() == 1
+    assert TextBlock.objects.first().title == 'Test Text'
 
-        self.assertFalse(TextBlock.text_objects.all().exists())
+
+def test_text_block_str(test_text_block: TextBlock):
+    text_block = TextBlock.objects.first()
+    assert str(text_block) == f'Text of {text_block.author}'
+
+
+def test_text_block_expiration_time(test_text_block: TextBlock):
+    text_block = TextBlock.objects.first()
+    assert text_block.expiration_time > timezone.now()
+
+
+def test_text_block_view_count(test_text_block: TextBlock):
+    text_block = TextBlock.objects.first()
+    assert text_block.view_count == 0
+
+
+def test_text_block_viewed_devices(test_text_block: TextBlock):
+    device = Device.objects.create(ip_address='192.168.1.1')
+    text_block = TextBlock.objects.first()
+    text_block.viewed_devices.add(device)
+    assert text_block.viewed_devices.count() == 1
+
+
+def test_text_block_manager(test_text_block: TextBlock):
+    assert TextBlock.active_objects.count() == 1
+    assert TextBlock.active_objects.count() == 1
