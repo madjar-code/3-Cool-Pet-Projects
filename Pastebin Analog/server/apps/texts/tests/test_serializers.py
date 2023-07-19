f77bfa94f7314fff84fa599d35771feed54b58ad
Madjar Evan <evanmadjar@gmail.com>
2023-07-19 20:56:06 +0300

    test: Test serializers using pytest

--- Pastebin Analog/server/apps/texts/tests/test_serializers.py
+++ Pastebin Analog/server/apps/texts/tests/test_serializers.py
@@ -1,73 +1,140 @@
-from typing import List, OrderedDict, Any
-from django.test import TestCase
-from mixer.backend.django import mixer
+import pytest
+from django.utils import timezone
+from typing import Dict, Any
+from rest_framework.test import APIClient
+from rest_framework.exceptions import ValidationError
 from users.models import User
 from texts.models import TextBlock
 from texts.api.serializers import (
     SimpleTextBlockSerializer,
-    TextBlockSerializer,
     CUTextBlockSerializer,
 )
 
 
-class TestTextBlockSerializers(TestCase):
-    def setUp(self) -> None:
-        self.user: User = User.objects.create_user(
-            username='test', email='test@test.com', password='test')
+@pytest.fixture
+def api_client() -> APIClient:
+    return APIClient()
 
-        self.text_block1: TextBlock = mixer.blend(
-            TextBlock, author=self.user, text=mixer.faker.text())
-        self.text_block2: TextBlock = mixer.blend(
-            TextBlock, author=self.user, text=mixer.faker.text())
-        self.text_block3: TextBlock = mixer.blend(
-            TextBlock, text=mixer.faker.text())
-        self.text_block4: TextBlock = mixer.blend(
-            TextBlock, text=mixer.faker.text())
 
-        self.simple_serializer = SimpleTextBlockSerializer(
-            instance=[self.text_block1, self.text_block2], many=True)
-        self.detail_serializer1 = TextBlockSerializer(
-            instance=self.text_block1)
-        self.detail_serializer2 = TextBlockSerializer(instance=self.text_block3)
+@pytest.fixture
+def test_user(db) -> User:
+    user = User.objects.create_user(
+        username='testuser', email='test@test.com', password='testpass')
+    return user
 
-        self.create_serializer1 = CUTextBlockSerializer(
-            data={'text': 'Test Text'})
-        self.create_serializer2 = CUTextBlockSerializer(
-            data={'text': 'Test Text', 'expiration_time': '2023-06-01T07:25:39.447Z'})
-        self.create_serializer3 = CUTextBlockSerializer(
-            data={'text': 'Test Text', 'author': self.user.id})
-        self.create_serializer4 = CUTextBlockSerializer(
-            data={'text': 'Test Text', 'expiration_time': 'wrong_time'})
-        
 
-    def test_simple_serializer(self):
-        serialized_data: List[OrderedDict[str, Any]] = self.simple_serializer.data
-        self.assertEqual(len(serialized_data), 2)
+@pytest.fixture
+def test_text_block(test_user: User) -> TextBlock:
+    text_block = TextBlock.objects.create(
+        title='Test Text',
+        text='This is a test text.',
+        author=test_user,
+    )
+    return text_block
 
-        self.assertEqual(list(serialized_data[0].keys()), ['id', 'author', 'hash'])
-        self.assertEqual(list(serialized_data[1].keys()), ['id', 'author', 'hash'])
 
-    def test_detail_serializer(self):
-        serialized_data: OrderedDict[str, Any] = self.detail_serializer1.data
-    
-        self.assertEqual(list(serialized_data.keys()),
-                         ['id', 'author', 'hash', 'text', 'expiration_time'])
+def test_simple_text_block_serializer(api_client: APIClient, test_text_block: TextBlock):
+    serializer = SimpleTextBlockSerializer(instance=test_text_block)
+    serialized_data: Dict[str, Any] = serializer.data
 
-    def test_create_serializer_only_text(self):
-        serializer: CUTextBlockSerializer = self.create_serializer1
-        self.assertTrue(serializer.is_valid())
+    assert serialized_data['id'] == str(test_text_block.id)
+    assert serialized_data['title'] == test_text_block.title
+    assert serialized_data['author'] == test_text_block.author.id
+    assert serialized_data['view_count'] == test_text_block.view_count
+    assert serialized_data['hash'] == test_text_block.hash
 
-    def test_create_serializer_text_exp_time(self):
-        serializer: CUTextBlockSerializer = self.create_serializer2
-        self.assertTrue(serializer.is_valid())
 
-    def test_create_serializer_text_author(self):
-        serializer: CUTextBlockSerializer = self.create_serializer3
-        self.assertTrue(serializer.is_valid())
+def test_cu_text_block_serializer_create(test_user: User):
+    serializer_data: Dict[str, Any] = {
+        'title': 'Test Text',
+        'text': 'This is a test text.',
+        'author': test_user.id,
+        'time_delta': 60,
+    }
 
-    def test_create_serializer_text_exp_time_wrong_data(self):
-        serializer: CUTextBlockSerializer = self.create_serializer4
-        self.assertFalse(serializer.is_valid())
-        self.assertIn('expiration_time', serializer.errors)
-        error_message = serializer.errors['expiration_time'][0]
-        self.assertEqual(error_message, 'Invalid date format')
+    serializer = CUTextBlockSerializer(data=serializer_data)
+    assert serializer.is_valid()
+
+    instance: TextBlock = serializer.save()
+
+    assert instance.title == serializer_data['title']
+    assert instance.text == serializer_data['text']
+    # assert instance.author == test_user
+    assert instance.view_count == 0
+    assert instance.expiration_time > timezone.now()
+
+
+def test_cu_text_block_serializer_create_no_time_delta(test_user: User):
+    serializer_data: Dict[str, Any] = {
+        'title': 'Test Text',
+        'text': 'This is a test text.',
+        'author': test_user.id,
+    }
+
+    serializer = CUTextBlockSerializer(data=serializer_data)
+    assert serializer.is_valid()
+
+    instance: TextBlock = serializer.save()
+
+    assert instance.title == serializer_data['title']
+    assert instance.text == serializer_data['text']
+    # assert instance.author == test_user
+    assert instance.view_count == 0
+    assert instance.expiration_time is None
+
+
+def test_cu_text_block_serializer_create_null_time_delta(test_user: User):
+    serializer_data: Dict[str, Any] = {
+        'title': 'Test Text',
+        'text': 'This is a test text.',
+        'author': test_user.id,
+        'time_delta': None,
+    }
+
+    serializer = CUTextBlockSerializer(data=serializer_data)
+    assert serializer.is_valid()
+
+    instance: TextBlock = serializer.save()
+
+    assert instance.title == serializer_data['title']
+    assert instance.text == serializer_data['text']
+    # assert instance.author == test_user
+    assert instance.view_count == 0
+    assert instance.expiration_time is None
+
+
+def test_cu_text_block_serializer_update(test_user: User):
+    text_block = TextBlock.objects.create(
+        title='Existing Text',
+        text='This is an existing text.',
+        author=test_user,
+    )
+
+    serializer_data: Dict[str, Any] = {
+        'title': 'Updated Text',
+        'text': 'This is an updated text.',
+        'time_delta': 120,
+    }
+
+    serializer = CUTextBlockSerializer(instance=text_block, data=serializer_data, partial=True)
+    assert serializer.is_valid()
+
+    instance: TextBlock = serializer.save()
+
+    assert instance.title == serializer_data['title']
+    assert instance.text == serializer_data['text']
+    # assert instance.author == test_user
+    assert instance.view_count == text_block.view_count
+    assert instance.expiration_time > timezone.now()
+
+
+def test_cu_text_block_serializer_invalid_time_delta(test_user: User):
+    serializer_data: Dict[str, Any] = {
+        'title': 'Test Text',
+        'text': 'This is a test text.',
+        'author': test_user.id,
+        'time_delta': 'invalid_time',
+    }
+
+    serializer = CUTextBlockSerializer(data=serializer_data)
+    assert not serializer.is_valid()
